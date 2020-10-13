42d524f7bae3aef0573bdeb994b9ff433bf8adc0
NathiMehlo <unathimehlomakulu@gmail.com>
2020-10-13 14:53:21 +0200

    Made some an import change

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/core';
-import { Response } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 
 @Injectable()
